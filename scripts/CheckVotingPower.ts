4d663d079729367b03713465a4af2051796f5c86
oxgreatape <0xgreatape@gmail.com>
2023-05-26 11:55:34 +0700

    added delegating voting power function

--- scripts/CheckVotingPower.ts
+++ scripts/CheckVotingPower.ts
@@ -1,9 +1,33 @@
 import { ethers } from "ethers";
+import * as dotenv from "dotenv";
+import {
+  YamatoTokenizedBallot__factory,
+  Yamato__factory,
+} from "../typechain-types";
+
+dotenv.config();
 
 async function main() {
   const provider = new ethers.providers.InfuraProvider(
     "goerli",
     process.env.INFURA_API_KEY
+  );
+  const privateKey = process.env.PRIVATE_KEY;
+  if (!privateKey || privateKey.length <= 0)
+    throw new Error("Error no private key, check .env file");
+  const wallet = new ethers.Wallet(privateKey);
+  const signer = wallet.connect(provider);
+
+  // get contract
+  const yamatoContractFactory = new Yamato__factory(signer);
+  const yamatoContract = yamatoContractFactory.attach(
+    String(process.env.YAMATO_CONTRACT_ADDRESS)
+  );
+  const votingPower = await yamatoContract.getVotes(signer.address);
+  console.log(
+    `The user: ${
+      signer.address
+    } has a voting power of ${ethers.utils.formatEther(votingPower)}`
   );
 }
 
--- scripts/DelegateVotingPower.ts
+++ scripts/DelegateVotingPower.ts
@@ -1,7 +1,35 @@
 import { ethers } from "ethers";
-
+import * as dotenv from "dotenv";
+import { Yamato__factory } from "../typechain-types";
+dotenv.config();
 async function main() {
-  
+  // setup wallet and provider
+  const provider = new ethers.providers.InfuraProvider(
+    "goerli",
+    process.env.INFURA_API_KEY
+  );
+  const privateKey = process.env.PRIVATE_KEY;
+  if (!privateKey || privateKey.length < 0) {
+    throw new Error("Error Invalid private key, check .env file");
+  }
+  const wallet = new ethers.Wallet(privateKey);
+  const signer = wallet.connect(provider);
+  // setup token contract
+  const yamatoContractFactory = new Yamato__factory(signer);
+  const yamatoContract = yamatoContractFactory.attach(
+    String(process.env.YAMATO_CONTRACT_ADDRESS)
+  );
+  try {
+    const delegateSelfTx = await yamatoContract
+      .connect(signer)
+      .delegate(signer.address);
+    const delegateTxReceipt = await delegateSelfTx.wait();
+    console.log(
+      `The tokens were delegated from the user ${signer.address} to itself at ${delegateTxReceipt.blockNumber}`
+    );
+  } catch (error) {
+    console.log(`Error, unable to delegate voting power: ${error}`);
+  }
 }
 
 main().catch((error) => {
